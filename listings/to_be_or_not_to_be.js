4dd402b9d63faddc43d1d962d71d33fb9b6da5a5
mpi <michal.piotrkowski@pragmatists.pl>
2017-05-08 11:52:50 +0200

    description of _.flow()

---
+++ listings/to_be_or_not_to_be.js
@@ -0,0 +1,37 @@
+module.exports = `
+To be, or not to be, that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune,
+Or to take Arms against a Sea of troubles,
+And by opposing end them: to die, to sleep
+No more; and by a sleep, to say we end
+the heart-ache, and the thousand natural shocks
+that Flesh is heir to? 'Tis a consummation
+devoutly to be wished. To die, to sleep,
+To sleep, perchance to Dream; aye, there's the rub,
+for in that sleep of death, what dreams may come,
+when we have shuffled off this mortal coil,
+must give us pause. There's the respect
+that makes Calamity of so long life:
+For who would bear the Whips and Scorns of time,
+the Oppressor's wrong, the proud man's Contumely, [F: poor]
+the pangs of despised Love, the Law’s delay, [F: disprized]
+the insolence of Office, and the spurns
+that patient merit of the unworthy takes,
+when he himself might his Quietus make
+with a bare Bodkin? Who would Fardels bear, [F: these Fardels]
+to grunt and sweat under a weary life,
+but that the dread of something after death,
+the undiscovered country, from whose bourn
+no traveller returns, puzzles the will,
+and makes us rather bear those ills we have,
+than fly to others that we know not of.
+Thus conscience does make cowards of us all,
+and thus the native hue of Resolution
+Is sicklied o'er, with the pale cast of Thought,
+And enterprises of great pitch and moment, [F: pith]
+with this regard their Currents turn awry, [F: away]
+And lose the name of Action. Soft you now,
+The fair Ophelia? Nymph, in thy Orisons
+Be all my sins remember'd.
+`;
